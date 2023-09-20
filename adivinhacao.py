fc8deb1dd52e2cf63b0b4341b385e20e7d842709
letciak <freitas.leticiak@gmail.com>
2023-09-20 17:32:21 -0300

    laço de repetição

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,21 +1,25 @@
-print("bem vindo ao jogo de Adivinhação!")
+print("bem vindo ao jogo de adivinhação!")
 
 numero_secreto = 42
+tentativa = 3
+rodada = 1
 
-chute_str = input("digite o seu número: ")
-print("você digitou " , chute_str)
-chute = int(chute_str)
+for rodada in range(1, tentativa + 1):
+    print("tentativa {} de {}".format(rodada, tentativa))
+    chute_str = input("digite o seu número: ")
+    print("você digitou " , chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("parabéns! você acertou!")
-else:
-    if(maior):
-        print("o seu chute foi maior do que o número secreto!")
-    elif(menor):
-        print("o seu chute foi menor do que o número secreto!")
+    if(acertou):
+        print("parabéns! você acertou!")
+    else:
+        if(maior):
+            print("o seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("o seu chute foi menor do que o número secreto!")    
 
-print("fim do jogo")
+    print("fim do jogo")
