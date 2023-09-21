8f439c9217f69f5a8715dc0048bac55c9201b1cf
letciak <freitas.leticiak@gmail.com>
2023-09-20 22:20:56 -0300

    import lib random

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,25 +1,48 @@
+import random
+
 print("bem vindo ao jogo de adivinhação!")
 
-numero_secreto = 42
-tentativa = 3
-rodada = 1
+numero_secreto = random.randrange(1, 101)
+tentativa = 0
+pontos = 1000
+
+print("qual nivel da dificuldade?")
+print("(1) facil (2) medio (3) dificil")
+
+nivel = int(input("defina o nivel: "))
+
+if(nivel == 1):
+    tentativa = 20
+elif(nivel == 2):
+    tentativa = 10
+else:
+    tentativa= 5
 
 for rodada in range(1, tentativa + 1):
     print("tentativa {} de {}".format(rodada, tentativa))
-    chute_str = input("digite o seu número: ")
+
+    chute_str = input("digite um número entre 1 e 100: ")
     print("você digitou " , chute_str)
     chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print("você deve digitar um número entre 1 e 100!")
+        continue
+
     acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
     if(acertou):
-        print("parabéns! você acertou!")
+        print("você acertou e fez {} pontos!".format(pontos))
+        break
     else:
         if(maior):
-            print("o seu chute foi maior do que o número secreto!")
+            print("você errou! o seu chute foi maior do que o número secreto.")
         elif(menor):
-            print("o seu chute foi menor do que o número secreto!")    
+            print("você errou! o seu chute foi menor do que o número secreto.")
 
-    print("fim do jogo")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos    
+
+print("fim do jogo")
