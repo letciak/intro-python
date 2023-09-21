818d62f62695d617cfbf72044373910188f6903c
letciak <freitas.leticiak@gmail.com>
2023-09-21 20:58:50 -0300

    add laço de repetição e funções com string

--- forca.py
+++ forca.py
@@ -2,6 +2,23 @@ def jogar():
 
     print("bem vindo ao jogo da forca!")
 
+    palavra_secreta = "python"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando")
+
     print("fim do jogo")
 
 if (__name__ == "__main__"):
